438acfd28b57fdc62ec82fc7975d143c337f3d98
RodrigoCF25 <rodrigocf88@hotmail.com>
2024-08-21 21:39:31 -0600

    Starting Log, Reading and Writing by HTTP requests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RodrigoCF25/0231637_SistemasDistribuidos
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+type Log struct {
+	mutex   sync.RWMutex
+	records []Record
+}
+
+type Record struct {
+	Value  []byte `json:"value"`
+	Offset uint64 `json:"-"`
+}
+
+// Constructor for Log
+func GetNewLog() *Log {
+	return &Log{
+		mutex:   sync.RWMutex{},
+		records: make([]Record, 0, 10),
+	}
+}
+
+// Method to append a record to the log
+func (l *Log) Append(record Record) {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	record.Offset = uint64(len(l.records))
+	fmt.Println("Record Appended: ", record)
+	l.records = append(l.records, record)
+}
+
+// Method to read a record from the log
+func (l *Log) Read(offset uint64) (Record, error) {
+	l.mutex.RLock()
+	defer l.mutex.RUnlock()
+
+	if offset >= uint64(len(l.records)) {
+		return Record{}, fmt.Errorf("offset out of range")
+	}
+
+	return l.records[offset], nil
+}
+
+// Stringer interface, basically the __repr__ of python
+func (l *Log) String() string {
+	l.mutex.RLock()
+	defer l.mutex.RUnlock()
+
+	return fmt.Sprintf("Log: %v", l.records)
+}
+
+func (r Record) String() string {
+	return fmt.Sprintf("{value: %s, offset: %d}", string(r.Value), r.Offset)
+}
+
+// Function to write to the log when a POST request is made to /write
+func WriteToLog(l *Log, w http.ResponseWriter, r *http.Request) {
+
+	// Check if the method is POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		fmt.Println("Writing to log")
+		return
+	}
+
+	var record Record
+
+	err := json.NewDecoder(r.Body).Decode(&record)
+
+	// Check if the body is empty or if record is not empty
+	if err != nil || len(record.Value) == 0 {
+		http.Error(w, "Error reading body", http.StatusBadRequest)
+		return
+	}
+
+	l.Append(record)
+}
+
+// Function to read from the log when a GET request is made to /read
+func ReadFromLog(l *Log, w http.ResponseWriter, r *http.Request) {
+
+	// Check if the method is GET
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	//Get the offset from the query and convert it to uint64
+	offset, err := strconv.ParseUint(r.URL.Query().Get("offset"), 10, 64)
+
+	// Check if the offset is invalid
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, "Invalid offset", http.StatusBadRequest)
+		return
+	}
+
+	// Read the record from the log
+	record, err := l.Read(offset)
+
+	// Check if the record is not found
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		http.Error(w, "Record not found", http.StatusNotFound)
+		return
+	}
+
+	// Encode the record to JSON and write it to the response
+	err = json.NewEncoder(w).Encode(record)
+
+	// Check if the encoding failed
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, "Error encoding record", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(record)
+
+}
+
+func main() {
+
+	const (
+		port      = ":8080"
+		writePath = "/write"
+		readPath  = "/read"
+	)
+
+	myLog := GetNewLog()
+
+	http.HandleFunc(writePath, func(w http.ResponseWriter, r *http.Request) {
+		WriteToLog(myLog, w, r)
+	})
+
+	http.HandleFunc(readPath, func(w http.ResponseWriter, r *http.Request) {
+		ReadFromLog(myLog, w, r)
+	})
+
+	log.Fatal(http.ListenAndServe(port, nil))
+
+}
